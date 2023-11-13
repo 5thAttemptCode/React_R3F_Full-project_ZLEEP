aa7a10566e8283c1722f6a308b78e73793c82878
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2023-11-13 20:25:17 +0100

    Add files via upload

--- src/Pages/Experience.jsx
+++ src/Pages/Experience.jsx
@@ -1,6 +1,9 @@
 import { Canvas } from '@react-three/fiber'
 import React from 'react'
-import { ScrollControls } from '@react-three/drei'
+import { Stars } from '@react-three/drei'
+import Background from '../ComponentsCanvas/Background'
+
+import { Perf } from 'r3f-perf'
 import Scene from '../ComponentsCanvas/Scene'
 
 
@@ -9,12 +12,15 @@ export default function Experience() {
 
 
   return (
-    <div className='canvas'>
-      <Canvas>
-        <ScrollControls pages={10} damping={1}>
+    <>
+      <div className='canvas'>
+        <Canvas>
+          <Background />
+          <Stars count={2000} />
           <Scene />
-        </ScrollControls>
-      </Canvas>
-    </div>
+          <Perf position="bottom-left" />
+        </Canvas>
+      </div>
+    </>
   )
 }
